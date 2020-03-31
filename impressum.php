e3a27fc89c8e557f5d7924e2a0c476890c107e2c
fabio <fabio>
2020-03-31 08:02:16 +0200

    updated impressum

--- impressum.php
+++ impressum.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>TIP YOUR LOCAL - REGISTRIEREN</title>
+    <title>TIP YOUR LOCAL - IMPRESSUM</title>
 
     <!-- Add icon library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/brands.min.css"></script>
@@ -35,7 +35,13 @@
 
     <script type="text/javascript" src="js/index.js"></script>
 
+    <style>
 
+        *{
+            text-align: left;   
+        }
+
+    </style>
 
 </head>
 
@@ -87,7 +93,7 @@
         </button></div>
 
         <div class="navbar-nav col">
-            <a class="navbar-brand" href="./index">TIP YOUR LOCAL</a>
+            <a class="navbar-brand" href="./index" style="text-align: center">TIP YOUR LOCAL</a>
         </div>
     </nav>
 
@@ -99,14 +105,26 @@
         <h1>Impressum</h1>
         <div style="height: 50px"></div>
 
+        <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+        <p>Fabio Sulser<br />
+        Sch&ouml;nhauser Allee<br />
+        175<br />
+        10119 Berlin</p>
+
         <h2>Kontakt</h2>
         <p>E-Mail: <a href="mailto:help@tipyourlocal.com" target="_blank">help@tipyourlocal.com</a></p>
 
         <h3>Haftung f&uuml;r Inhalte</h3> <p>Als Diensteanbieter sind wir gem&auml;&szlig; &sect; 7 Abs.1 TMG f&uuml;r eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach &sect;&sect; 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, &uuml;bermittelte oder gespeicherte fremde Informationen zu &uuml;berwachen oder nach Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit hinweisen.</p> <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unber&uuml;hrt. Eine diesbez&uuml;gliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung m&ouml;glich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p> <h3>Haftung f&uuml;r Links</h3> <p>Unser Angebot enth&auml;lt Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb k&ouml;nnen wir f&uuml;r diese fremden Inhalte auch keine Gew&auml;hr &uuml;bernehmen. F&uuml;r die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf m&ouml;gliche Rechtsverst&ouml;&szlig;e &uuml;berpr&uuml;ft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.</p> <p>Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p> <h3>Urheberrecht</h3> <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielf&auml;ltigung, Bearbeitung, Verbreitung und jede Art der Verwertung au&szlig;erhalb der Grenzen des Urheberrechtes bed&uuml;rfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur f&uuml;r den privaten, nicht kommerziellen Gebrauch gestattet.</p> <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+
+        </br>
+        <p>Quelle: <a href="https://www.e-recht24.de" rel="nofollow">e-recht24.de</a></p>
     </div>
 
     <div style="height: 100px"></div>
-    <footer class="page-footer font-small blue">
+
+
+</div>
+<footer class="page-footer font-small blue">
         <a class="btn btn_sm" id="sidebarCollapse" href="https://www.facebook.com/tipyourlocal">
             <i class="fab fa-facebook fa-2x"></i>
         </a>
@@ -117,8 +135,5 @@
             <i class="fab fa-linkedin fa-2x"></i>
         </a>
     </footer>
-
-</div>
-
 </body>
 </html>
